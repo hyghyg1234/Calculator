a7e1ea82d8594221196161295a869d6c688c4723
michaelhu <hyghyg126@126.com>
2017-06-21 17:07:30 +0800

    修改使用LIST管理按键。

--- 计算器/Form1.Designer.cs
+++ 计算器/Form1.Designer.cs
@@ -28,12 +28,280 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.num1 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.num0 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(6, 5);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(35, 35);
+            this.num1.TabIndex = 0;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(47, 5);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(35, 35);
+            this.num2.TabIndex = 1;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(88, 5);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(35, 35);
+            this.num3.TabIndex = 2;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(129, 5);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(35, 35);
+            this.num4.TabIndex = 3;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(6, 45);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(35, 35);
+            this.num5.TabIndex = 4;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(47, 45);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(35, 35);
+            this.num6.TabIndex = 5;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(88, 45);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(35, 35);
+            this.num7.TabIndex = 6;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            // 
+            // num8
+            // 
+            this.num8.Location = new System.Drawing.Point(129, 45);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(35, 35);
+            this.num8.TabIndex = 7;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            // 
+            // num9
+            // 
+            this.num9.Location = new System.Drawing.Point(6, 85);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(35, 35);
+            this.num9.TabIndex = 8;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(6, 124);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(35, 35);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "+";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(47, 124);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(35, 35);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "-";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(88, 124);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(35, 35);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "*";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(129, 124);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(35, 35);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "/";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(170, 124);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(35, 35);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "=";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // num0
+            // 
+            this.num0.Location = new System.Drawing.Point(47, 85);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(35, 35);
+            this.num0.TabIndex = 14;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(88, 85);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(35, 35);
+            this.button16.TabIndex = 15;
+            this.button16.Text = ".";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(129, 85);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(76, 34);
+            this.button17.TabIndex = 16;
+            this.button17.Text = "backspace";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 15F);
+            this.textBox1.Location = new System.Drawing.Point(21, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 30);
+            this.textBox1.TabIndex = 17;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 15F);
+            this.textBox2.Location = new System.Drawing.Point(14, 226);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 30);
+            this.textBox2.TabIndex = 19;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button17);
+            this.panel1.Controls.Add(this.button16);
+            this.panel1.Controls.Add(this.num0);
+            this.panel1.Controls.Add(this.button14);
+            this.panel1.Controls.Add(this.num4);
+            this.panel1.Controls.Add(this.button13);
+            this.panel1.Controls.Add(this.num1);
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.num2);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.num3);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.num5);
+            this.panel1.Controls.Add(this.num9);
+            this.panel1.Controls.Add(this.num6);
+            this.panel1.Controls.Add(this.num8);
+            this.panel1.Controls.Add(this.num7);
+            this.panel1.Location = new System.Drawing.Point(14, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(216, 166);
+            this.panel1.TabIndex = 20;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("宋体", 15F);
+            this.textBox3.Location = new System.Drawing.Point(261, 226);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(199, 30);
+            this.textBox3.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 268);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- 计算器/Form1.cs
+++ 计算器/Form1.cs
@@ -15,5 +15,130 @@ namespace 计算器
         {
             InitializeComponent();
         }
+
+        string[] num = new string[100];
+        string flag;
+        Point panel1Position;
+
+        List<Button> butList = new List<Button>();
+        List<string> numList = new List<string>();
+
+        private void list_Add()
+        {
+            butList.Add(num0);
+            butList.Add(num1);
+            butList.Add(num2);
+            butList.Add(num3);
+            butList.Add(num4);
+            butList.Add(num5);
+            butList.Add(num6);
+            butList.Add(num7);
+            butList.Add(num8);
+            butList.Add(num9);            
+            butList.Add(button16);
+            for (int i = 0; i < 11; i++)
+            {
+                butList[i].Click += new System.EventHandler(this.butlist_Click);
+                butList[i].MouseDown += new System.Windows.Forms.MouseEventHandler(this.butlist_MouseDown);
+            }
+        }
+
+        Point buttonPoint = new Point(1, 2);
+
+        private void butlist_MouseDown(object sender, MouseEventArgs e)
+        {
+            buttonPoint.X = e.X;
+            buttonPoint.Y = e.Y;
+            textBox2.Text = (buttonPoint.X).ToString() + "    " + (buttonPoint.Y).ToString();
+        }
+
+        private void butlist_Click(object sender, EventArgs e)
+        {
+            //鼠标相对于屏幕的坐标
+            Point p1 = MousePosition;
+            //鼠标相对于窗体的坐标
+            Point p2 = this.PointToClient(MousePosition);
+
+            //计算出按键相对于panel的坐标
+            int PointX = p2.X - panel1Position.X;   
+            int PointY = p2.Y - panel1Position.Y;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if ((PointX >= butList[i].Location.X) && (PointX <= butList[i].Location.X + 35) && (PointY >= butList[i].Location.Y) && (PointY <= butList[i].Location.Y + 35))
+                {
+                    textBox3.Text = PointX.ToString() + "   " + PointY.ToString();
+                    num[but_flag] = num[but_flag] + i.ToString();
+                    textBox1.Text = num[but_flag];
+                }
+            }
+            
+            
+
+            textBox2.Text = this.butList[0].Location.ToString();
+            
+        }
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    num[but_flag] = num[but_flag] + "2";
+        //    textBox1.Text = num[but_flag];       
+        //}      
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int but_flag = 0;
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            but_flag++;
+            flag = "+";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            but_flag++;
+            flag = "-";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            but_flag++;
+            flag = "*";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            but_flag++;
+            flag = "/";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            int res = 0;
+            if (flag == "+")
+            {
+                while (but_flag >= 0)
+                {
+                    res =res+ Convert.ToInt16(num[but_flag]);
+                    but_flag--;
+                }              
+            }
+            textBox1.Text = res.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list_Add();
+            panel1Position = this.panel1.Location;
+            textBox2.Text = panel1Position.ToString();
+        }
     }
 }
